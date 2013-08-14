34785b02c69490e4ceb4c17a6c0f7e9579cb2a98
Joel Duffin <joel.duffin@gmail.com>
2013-08-14 16:04:22 -0600

    initial project commit

---
+++ public/help.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+
+<title>Help</title>
+<meta name="description" content="">
+
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="css/style.css">
+
+<script src="js/libs/modernizr.js"></script>
+</head>
+<body>
+  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+<header class="masthead">
+        <input type="button" name="closeHelp" id="closeHelp" value="close | x" onclick="window.close()">
+</header>
+
+<div id="wrapper">
+    <div role="main" id="container">
+		<div class="content">
+            <div id="information">
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.topBarInformation {border-bottom:1px dashed #c0c0c0;width:100%;}
+						.topBarInformation a {color:#c0c0c0;}
+						.topBarInformation h1 {padding:20px 0px 10px 25px;}
+					</style>
+					<!-- Toolbar for the Information view-->
+					<div class="topBarInformation">
+						<h1>Information help</h1>
+					</div>
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.viewHelp {padding:25px;}
+					</style>
+					<div class="viewHelp">
+						<h2>Suggested Sections</h2>
+							<div>At the minimum, in the <strong>Information</strong> portion of your syllabus provide:<br /><br /></div>
+								<h3>Syllabus Title</h3>
+									<div>The course title, the course number, and the current term designation.<br /><br /></div>
+								<h3>Contact Information (Section 1)</h3>
+									<div>Your title and primary contact method. Provide information about your communication preferences --when you will check email, how quickly you will respond, etc. --in the Course Policies section.<br /><br /></div>
+									<div><a href="">View Policies help</a><br /><br /></div>
+									<div>If you are team teaching and/or have a Teaching Assistant, use the &#8221;add contact&#8221; tool to add up to three additional Contacts.<br /><br /></div>
+								<h3>Course Description (Section 3)</h3>
+									<div>A description of your course. Here are some suggested formats:
+										<ul>
+											<li>Objective-based narrative: link together your Objectives to tell the &#8221;story&#8221; of your course</li>
+											<li>Questions: pose a series of questions that the course will help your student answer</li>
+											<li>Anecdote: give an example from your experience that engages the student and creates
+												a context for learning</li>
+										</ul>
+									</div>
+					<h2>Additional Sections</h2>
+							<div>Use the &#8220;Section 2&#8221; tool to show a new section before the <strong>Course Description</strong>. Use
+							the &#8221;Section 4&#8221; and &#8221;Section 5&#8221; tools to place new sections after the <strong>Course Description</strong>.</div>
+						<div class="topBarInformation">&nbsp;</div>
+					</div><!-- viewHelp-->
+			</div><!-- #information -->
+			
+			<div id="outcomes">
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.topBarOutcomes {border-bottom:1px dashed #7FC5EC;width:100%;}
+						.topBarOutcomes a {color:#7FC5EC;}
+						.topBarOutcomes h1 {padding:20px 0px 10px 25px;}
+					</style>
+					<!-- Toolbar for the Information view-->
+					<div class="topBarOutcomes">
+						<h1>Outcomes help</h1>
+					</div>
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.viewHelp {padding:25px;}
+					</style>
+					<div class="viewHelp">
+						<h2>OAR model</h2>
+							<div>The Syllabus Builder uses the Objectives-Resources-Activities (OAR) model as a development framework.<br /><br /></div>
+							<div><a href="http://www.oarmodel.com" target="_blank">Learn more about the OAR model</a><br /><br /></div>
+							<div><strong>Objectives</strong> are statements created to define and/or measure desired student behavior. <strong>Learning Outcomes</strong> are statements that address the student directly, and incorporate an action verb.<br /><br /></div>
+							<div>The default design of the <strong>Learning Outcomes</strong> section uses a common "stem" for all outcomes:<br /><br /></div>
+							<div>You may modify this stem by highlighting and typing over the text.* You may also delete this stem if you would prefer to use different stems which are embedded in each Outcome.<br /><br /></div>
+								<h3>Bloom's Revised Taxonomy</h3>
+									<div>The &#8220;Bloom's Revised&#8221; tool incorporates six levels of learning: remember, understand, apply, analyze, evaluate and create. Clicking on a level opens a window with a group of action verb buttons that describe behaviors associated with the level. Clicking on a button inserts the action verb into a text field.<br /><br /></div>
+						<h2>Additional Sections</h2>
+							<div>Use the &#8220;Section 2&#8221; tool to add a new section after the <strong>Learning Outcomes.</strong></div>
+						<div class="topBarOutcomes">&nbsp;</div>
+					</div><!-- viewHelp-->
+			</div><!-- #outcomes -->
+			
+			<div id="resources">
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.topBarResources {border-bottom:1px dashed #F0E22E;width:100%;}
+						.topBarResources a {color:#F0E22E;}
+						.topBarResources h1 {padding:20px 0px 10px 25px;}
+					</style>
+					<!-- Toolbar for the Information view-->
+					<div class="topBarResources">
+						<h1>Resources help</h1>
+					</div>
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.viewHelp {padding:25px;}
+					</style>
+					<div class="viewHelp">
+						<h2>OAR model</h2>
+								<div>The Syllabus Builder uses the Objectives-Resources-Activities (OAR) model as a development framework.<br /><br /></div>
+								<div><a href="http://www.oarmodel.com" target="_blank">Learn more about the OAR model</a><br /><br /></div>
+								<div><strong>Resources</strong> are components available to influence and/or measure student behavior.<strong>Learning Outcomes</strong> are statements that address the student directly, and incorporate an action verb.<br /><br /></div>
+						<h2><br />Suggested Sections</h2>
+							<div>At the minimum, in the <strong>Learning Resources</strong> portion of your syllabus provide:<br /><br /></div>
+								<h3>Learning Management System</h3>
+									<div>The course title, the course number, and the current term designation.<br /><br /></div>
+								<h3>Techinical Support</h3>
+									<div>Your title and primary contact method. Provide information about your communication preferences --when you will check email, how quickly you will respond, etc. --in the Course Policies section.<br /><br /></div>
+									<div><a href="">View Policies help</a><br /><br /></div>
+								<h3>Required Software</h3>
+									<div>A detailed list of all software needed for the course. DO NOT assume that students will have commonly used software such as Microsoft Word or Microsoft PowerPoint. You may wish to provide information about open-source alternatives such as OpenOffice Writer.<br /><br /></div>
+									<div><a href="http://www.openoffice.org/" target="_blank">Visit the Open Office website</a><br /><br /></div>
+									<div>If you are team teaching and/or have a Teaching Assistant, use the &#8221;add contact&#8221; tool to add up to three additional Contacts.<br /><br /></div>
+								<h3>Textbook and/or other Reading Materials</h3>
+									<div>Information about textbooks, course packets, journal articles, and other reading materials	used in the course. Details such as textbook ISDN number, where to purchase reading materials and access information for password-protected digital assets.<br /><br /></div>
+								<h3>Videos</h3>
+									<div>Information about videos and/or other multimedia that will be used in the course. Also information about where (LMS, external website, etc.) and how (login information) students will access these resources. Information on software needed to run multimedia (Flash, HTML 5 browser, etc.) in <em>Required Software.</em><br /><br /></div>
+					<h2>Additional Sections</h2>
+							<div>Use the &#8220;Section 2&#8221; tool to add a new section after <strong>Learning Resources</strong> portion of your syllabus.</div>
+						<div class="topBarResources">&nbsp;</div>
+					</div><!-- viewHelp-->
+			</div><!-- #resources -->
+			
+			<div id="activities">
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.topBarActivities {border-bottom:1px dashed #A1CC3A;width:100%;}
+						.topBarActivities a {#A1CC3A;}
+						.topBarActivities h1 {padding:20px 0px 10px 25px;}
+					</style>
+					<!-- Toolbar for the Information view-->
+					<div class="topBarActivities">
+						<h1>Activities help</h1>
+					</div>
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.viewHelp {padding:25px;}
+					</style>
+					<div class="viewHelp">
+						<h2>OAR model</h2>
+							<div>The Syllabus Builder uses the Objectives-Resources-Activities (OAR) model as a development framework.<br /><br /></div>
+							<div><a href="http://www.oarmodel.com" target="_blank">Learn more about the OAR model</a><br /><br /></div>
+							<div><strong>Acitivites</strong> are opportunities to created to influence and/or measure defined student behavior. <strong>Learning Outcomes</strong> are combined with <strong>Learning Resources</strong> to create <strong>Learning Activities</strong>. There are two types of <strong>Learning Activities:</strong><br /></div>
+								<ul>
+									<li><strong>Application Activities</strong> are opportunities for students to demonstrate knowledge and skills</li>
+									<li><strong>Acquisition Activities</strong> are opportunities for students to gain knowledge and skills</li>
+								</ul>
+							<div>Provide general information on each <strong>Learning Activity</strong>: type, frequency and relationship to other <strong>Learning Activities</strong>.<br /></div>
+						<h2><br />Suggested Sections</h2>
+							<div>At the minimum, in the <strong>Activities</strong> portion of your syllabus provide:<br /><br /></div>
+								<h3>Readings</h3>
+									<div>Text<br /><br /></div>
+								<h3>Videos</h3>
+									<div>Text<br /><br /></div>
+								<h3>Discussions</h3>
+									<div>Text<br /><br /></div>
+								<h3>Assignments</h3>
+									<div>Text<br /><br /></div>
+								<h3>Quizzes</h3>
+									<div>Text<br /><br /></div>
+								<h3>Exams</h3>
+									<div>Text<br /><br /></div>
+					<h2>Additional Sections</h2>
+							<div>Use the &#8220;Section 2&#8221; and &#8220;Section 3&#8221; tools to place new sections after the <strong>Learning Activities</strong>.</div>
+						<div class="topBarActivities">&nbsp;</div>
+					</div><!-- viewHelp-->
+			</div><!-- #activities -->
+			
+			<div id="policies">
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.topBarPolicies {border-bottom:1px dashed #BEBEE0;width:100%;}
+						.topBarPolicies a {color:#BEBEE0;}
+						.topBarPolicies h1 {padding:20px 0px 10px 25px;}
+					</style>
+					<!-- Toolbar for the Information view-->
+					<div class="topBarPolicies">
+						<h1>Policies help</h1>
+					</div>
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.viewHelp {padding:25px;}
+					</style>
+					<div class="viewHelp">
+						<h2>Suggested Headings</h2>
+							<div>At the minimum, in the <strong>Course Policies</strong> section of your syllabus provide:<br /><br /></div>
+							<h3>Instructor Feedback/Communication</h3>
+									<div>The types of feedback and communication the instructor will provide, and which communication tools will be used. The response time for each type and tool.<br /><br /></div>
+								<h3>Student Feedback/Communication</h3>
+									<div>The types of feedback and communication the instructor will accept, and which communication tools will be used. The response time for each type and tool.<br /><br /></div>
+								<h3>Syllabus Changes</h3>
+									<div>The actions you will take if the syllabus must be revised. The actions students should take if there is a discrepancy between the syllabus, and other sources of information, i.e. - the learning management system.<br /><br /></div>
+								<h3>Late Work</h3>
+									<div>Your expectations regarding late work:<br />
+										<ul>
+											<li>Acceptable circumstances</li>
+											<li>Student responsibilties</li>
+											<li>Penalties</li>
+										</ul></div>
+								<h3>Academic Honesty</h3>
+									<div>A thorough definition of academic dishonesty, and the actions that you will take in response to confirmed violation.<br /><br /></div>
+								<h3>Students with Disabilities</h3>
+									<div>The process by which students with disabilities will receive assistance.<br /><br /></div>
+					<h2>Additional Sections</h2>
+							<div>Use the &#8220;Section 2&#8221; and &#8220;Section 3&#8221; tools to add sections that distinguish between policies required by different organizational levels, such as:<br />
+								<ul>
+											<li>Institution</li>
+											<li>College</li>
+											<li>Department</li>
+											<li>Program</li>
+										</ul></div>
+						<div class="topBarPolicies">&nbsp;</div>
+					</div><!-- viewHelp-->
+			</div><!-- #policies -->
+			
+			<div id="grades">
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.topBarGrades {border-bottom:1px dashed #D4DAC8;width:100%;}
+						.topBarGrades a {#D4DAC8;}
+						.topBarGrades h1 {padding:20px 0px 10px 25px;}
+					</style>
+					<!-- Toolbar for the Information view-->
+					<div class="topBarGrades">
+						<h1>Grades help</h1>
+					</div>
+					<!-- Move to syllabus.css -->
+					<style type="text/css">
+						.viewHelp {padding:25px;}
+					</style>
+					<div class="viewHelp">
+						<h2><br />Sections</h2>
+							<div>At the minimum, in the <strong>Information</strong> portion of your syllabus provide:<br /><br /></div>
+								<h3>Components</h3>
+									<div>Components are point-based <strong>Learning Activities</strong>. In the first column type in the component name and quantity/points per (if needed). In the second column type in the amount of points for the component. &#8220;Total Points&#8221; are calculated automatically.<br /><br /></div>
+								<h3>Scale</h3>
+									<div>The Scale is calculated automatically based on &#8220;Percentages&#8221; and &#8220;Total Points&#8221;.<br /><br /></div>
+						<div class="topBarGrades">&nbsp;</div>
+					</div><!-- viewHelp-->
+			</div><!-- #grades -->
+			
+		</div><!-- content -->
+	</div><!-- #container -->
+</div><!-- #wrapper -->
+
+<footer>    
+    <nav>
+        <ul>
+            <li><a href="http://usu.edu">Utah State University</a></li>
+            <li><a href="http://cidi.usu.edu">CIDI</a></li>
+        </ul>
+    </nav>
+    <a class="home" href="./index.php">Syllabus Builder</a>
+</footer>
+
+<div id="preview_control"><a href="#close">close | x</a></div>
+<div id="preview"></div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+<script>window.jQuery.fn.dialog || document.write('<script src="js/libs/jquery-ui-1.8.21.min.js"><\/script>')</script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$("#table1").tableDnD({
+	    onDragClass: "myDragClass",
+	});
+});
+</script>
+<script src="js/libs/tiny_mce/jquery.tinymce.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/script.js"></script>
+<script src="js/jquery.tablednd.0.7.min.js"></script>
+
+</body>
+</html>
