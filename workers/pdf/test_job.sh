307d78f28ae96bab6b322faace136bed4b528ff8
Joel Duffin <joel.duffin@gmail.com>
2013-08-26 11:34:57 -0600

    shell script for running a test job

---
+++ workers/pdf/test_job.sh
@@ -0,0 +1,1 @@
+curl --data "url=http://www.justinball.com/" "https://worker-aws-us-east-1.iron.io:443/2/projects/5217d1d38fe6380005000015/tasks/webhook?code_name=pdf_generator&oauth=Zztb-nslUMyUyWTEIN5YEwbZQbc"
