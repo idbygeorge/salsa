f129848256734fbda4b2370ea28122cb5b4266eb
Joel Duffin <joel.duffin@gmail.com>
2013-08-27 07:14:41 -0600

    precompile the start page css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Salsa::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( start.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
