a8328358c44d56f7afa4679c9b16aabad46f3623
Bowen Masco <bowen@codingfoo.com>
2014-01-25 20:57:11 -0700

    add capistrano

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+role :app, %w{ubuntu@ec2-54-201-41-191.us-west-2.compute.amazonaws.com}
+role :web, %w{ubuntu@ec2-54-201-41-191.us-west-2.compute.amazonaws.com}
+role :db,  %w{ubuntu@ec2-54-201-41-191.us-west-2.compute.amazonaws.com}
+
