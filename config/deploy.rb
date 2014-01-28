ab9d570f70fbd52d7c467739d8ba6ba13b24af6e
Bowen Masco <bowen@codingfoo.com>
2014-01-27 17:28:10 -0700

    link files

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/config.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -39,8 +39,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute "kill -s USR2 `cat /tmp/unicorn.salsa.pid`"
     end
   end
 
