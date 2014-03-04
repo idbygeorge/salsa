0e5a02e67cc11445329a37ec88ddde5065cc4e18
John Pope <pope@go.ceu.edu>
2014-03-04 13:05:38 -0700

    Remove all styling, fix auto size of HTML editor control

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -191,21 +191,33 @@ function liteOff(x){
             element.html($("<textarea/>").attr("id", "contentTextControl").val(text)).find("textarea");
             element.append($("<div id='old_html'>" + text + '</div>'));
 
+            var editorMaxHeight = $('body').innerHeight() * .8;
 
             $('#contentTextControl', this).tinymce({
-                toolbar: "bold italic underline | undo redo | bullist numlist",
+                toolbar: "bold italic underline | undo redo | bullist numlist | link unlink",
                 statusbar: false,
                 menubar : false,
 
-                plugins : "autoresize,autolink,paste",
-                autoresize_min_height: "2em",
+                plugins : "autoresize,link,autolink,paste",
+                
+                autoresize_max_height: editorMaxHeight,
                 paste_as_text: true,
+                //paste_word_valid_elements: "b,strong,i,em,h1,h2,h3,h4",
 
                 content_css : "/stylesheets/content.css",
                 width: '300',
                 height: '400',
                 auto_focus: 'contentTextControl',
                 setup : function(ed) {
+                    ed.on('focus', function(e){
+                        var maxHeight = $("body", this.contentDocument).height();
+
+                        this.theme.resizeTo(
+                            '100%',
+                            Math.min(editorMaxHeight, maxHeight)
+                        );
+                    });
+
                     ed.on('blur', function(e){
                         ed.remove();
                         $(".editingHtml textarea").blur();
