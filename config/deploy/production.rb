decb3882c00d39029b075de281226fe593175c49
John Pope <john.pope@usu.edu>
2014-03-17 12:53:59 -0600

    Remove deploy files from repository so they can be configured independently for each deploy target/instory so they can be configured independently for each deploy target/instance

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-role :app, %w{ubuntu@ec2-54-201-41-191.us-west-2.compute.amazonaws.com}
-role :web, %w{ubuntu@ec2-54-201-41-191.us-west-2.compute.amazonaws.com}
-role :db,  %w{ubuntu@ec2-54-201-41-191.us-west-2.compute.amazonaws.com}
-
