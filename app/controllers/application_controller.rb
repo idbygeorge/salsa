9347ca7c92856718167dea1093fcde476c5d2e52
John Pope <john.pope@usu.edu>
2014-03-27 13:05:49 -0600

    Allow for multiple domains logging in to canvas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  protected
+
   def init_view_folder
     # establish the default view folder
     @view_folder = "instances/default"
@@ -17,5 +19,35 @@ class ApplicationController < ActionController::Base
         @view_folder = "instances/custom/#{@organization.slug}"
       end
     end
+  end
+
+  def lms_connection_information
+    if session[:institution] && session[:institution] != ''
+      @institution = session['institution']
+    elsif !params[:institution] || params[:institution] == ''
+      @institution = request.env['SERVER_NAME']
+      @callback_url = callback_url
+    else
+      @institution = params[:institution]
+    end
+
+    @organization = Organization.find_by slug: @institution
+
+    # custom authentication source, use the keys from the DB
+    if @organization && @organization.lms_authentication_source
+      @oauth_endpoint = @organization.lms_authentication_source
+      @lms_client_id = @organization.lms_authentication_id
+      @lms_secret = @organization.lms_authentication_key
+      @callback_url = 'http://' + @organization[:slug] + '/oauth2/callback'
+    end
+
+    # defaults
+    @oauth_endpoint ||= "https://#{@institution}.instructure.com"
+    @lms_client_id ||= APP_CONFIG['canvas_id']
+    @lms_secret ||= APP_CONFIG['canvas_key']
+    @callback_url ||= APP_CONFIG['domain'] + '/oauth2/callback'
+
+    @lms_client = Canvas::API.new(:host => @oauth_endpoint, :client_id => @lms_client_id, :secret => @lms_secret)
+    @redirect_url = "#{@lms_client.oauth_url(@callback_url)}%3Fsyllabus_id%3D#{params[:syllabus_id]}"
   end
 end
--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -1,26 +1,17 @@
 class Oauth2Controller < ApplicationController
+  before_filter :lms_connection_information, only: [:login, :callback]
   def login
-    if !params[:institution]
-      institution = 'usu'
-    else
-      institution = params[:institution]
-    end
+    session[:institution] = @institution
+    session[:oauth_endpoint] = @oauth_endpoint
 
-    oauth_endpoint = "https://#{institution}.instructure.com"
-    
-    session[:institution] = params[:institution]
-    session[:oauth_endpoint] = oauth_endpoint
-
-    lms_client = Canvas::API.new(:host => oauth_endpoint, :client_id => APP_CONFIG['canvas_id'], :secret => APP_CONFIG['canvas_key'])
-
-    url = "#{lms_client.oauth_url(APP_CONFIG['oauth_callback_uri'])}%3Fsyllabus_id%3D#{params[:syllabus_id]}"
-    redirect_to(url)
+    redirect_to(@redirect_url)
   end
 
   def logout
     session[:institution] = ''
     session[:oauth_endpoint] = ''
     session[:canvas_access_token] = ''
+    session[:authenticated_institution] = ''
 
     flash[:notice] = "This application has been detached from canvas"
 
@@ -31,26 +22,15 @@ class Oauth2Controller < ApplicationController
     code = params[:code]
 
     if code
-      lms_client = Canvas::API.new(:host => session[:oauth_endpoint], :client_id => APP_CONFIG['canvas_id'], :secret => APP_CONFIG['canvas_key'])
-
-      url = "#{lms_client.oauth_url(APP_CONFIG['oauth_callback_uri'])}%3Fsyllabus_id%3D#{params[:syllabus_id]}"
-      token = lms_client.retrieve_access_token(code, url)
+      token = @lms_client.retrieve_access_token(code, @redirect_url)
       session[:canvas_access_token] = token
-      #TODO: set logged in
 
       flash[:notice] = 'Canvas authentication successful'
-
       session[:authenticated_institution] = session[:institution]
     else
       flash[:error] = params[:error]
     end
 
     redirect_to syllabus_path(params[:syllabus_id]) + '#/compilation/clipboard';
-  end
-
-  protected
-
-  def canvas_client
-    Canvas::API.new(:host => APP_CONFIG['canvas_api_endpoint'], :client_id => APP_CONFIG['canvas_id'], :secret => APP_CONFIG['canvas_key'])
   end
 end
