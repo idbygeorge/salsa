ab464e3848fdb012d94b5c8acb1de0963c588d11
John Pope <john.pope@usu.edu>
2014-04-04 14:42:33 -0600

    add new relic monitoring

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/config.yml}
+set :linked_files, %w{config/database.yml config/config.yml config/newrelic.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
