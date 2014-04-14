3d8641a51bcd0a42c75905c41ec85e4eacbeeb92
John Pope <john.pope@usu.edu>
2014-04-14 11:13:35 -0600

    add start.css precompile back in for production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Salsa::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
   #config.assets.precompile += ['*.js', '*.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
