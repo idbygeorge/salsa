11791f0ed13186d4ea152b39bcf31affcbfc7047
John Pope <john.pope@usu.edu>
2014-04-21 13:51:08 -0600

    add in vestal versions gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410105425) do
+ActiveRecord::Schema.define(version: 20140421194824) do
 
   create_table "organizations", force: true do |t|
     t.string   "name"
@@ -51,5 +51,26 @@ ActiveRecord::Schema.define(version: 20140410105425) do
   add_index "syllabuses", ["organization_id"], name: "index_syllabuses_on_organization_id"
   add_index "syllabuses", ["template_id"], name: "index_syllabuses_on_template_id", unique: true
   add_index "syllabuses", ["view_id"], name: "index_syllabuses_on_view_id", unique: true
+
+  create_table "versions", force: true do |t|
+    t.integer  "versioned_id"
+    t.string   "versioned_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "user_name"
+    t.text     "modifications"
+    t.integer  "number"
+    t.integer  "reverted_from"
+    t.string   "tag"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "versions", ["created_at"], name: "index_versions_on_created_at"
+  add_index "versions", ["number"], name: "index_versions_on_number"
+  add_index "versions", ["tag"], name: "index_versions_on_tag"
+  add_index "versions", ["user_id", "user_type"], name: "index_versions_on_user_id_and_user_type"
+  add_index "versions", ["user_name"], name: "index_versions_on_user_name"
+  add_index "versions", ["versioned_id", "versioned_type"], name: "index_versions_on_versioned_id_and_versioned_type"
 
 end
