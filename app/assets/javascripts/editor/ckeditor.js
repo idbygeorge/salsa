4e19358009bd1afff88d95114f54b6ff7a67f935
John Pope <john.pope@usu.edu>
2014-05-06 19:49:17 -0600

    fix ckeditor instances not being enabled by default on other tabs

--- app/assets/javascripts/editor/ckeditor.js
+++ app/assets/javascripts/editor/ckeditor.js
@@ -21,6 +21,10 @@ function CKEditor_cleanup(context) {
 // init the CKEditor - needs to set contenteditable on each element that needs the editor
 // or it has annoying side effects (inline editor doesn't go away on first blur, paste gets messed up)
 function CKEditor_init(context) {
+  // HACK: editors aren't active on other tabs... probably something with CKEditor that doesn't init them if the are hidden
+  $activeTab = jQuery("#page section.active");
+  jQuery("#page section").addClass('active');
+
   // set contenteditable to true for each element that the CKEditor needs to initialize
   jQuery('.editableHtml', context).attr('contenteditable', true).each(function(){
     // make sure there isn't an editor on this element already
@@ -34,4 +38,9 @@ function CKEditor_init(context) {
       // probably needs to be in the on init handler for this editor...
     }
   });
+
+  // complete the hack
+  setTimeout(function() {
+    jQuery('#page section').not($activeTab).removeClass('active');
+  }, 100);
 }
