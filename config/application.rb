9397402f9d87a5edda71f76901c7e0f9bd42cc82
John Pope <john.pope@usu.edu>
2014-05-09 08:24:14 -0600

    move chrome frame to header (bootstrap templates)

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,9 @@ module Salsa
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_dispatch.default_headers = {
+      'X-UA-Compatible' => 'IE=Edge,chrome=1'
+    }
   end
 end
