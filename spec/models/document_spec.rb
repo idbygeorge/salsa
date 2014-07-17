7bc693d33ad6d58b9d8a9e25550474b6c0e50aaf
John Pope <github@popeit.com>
2014-07-17 01:39:06 -0600

    document view, edit and template ids consist of 30 a-z characters

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Document, :type => :model do
   it "generates random IDs when creating" do
   	document = Document.create!(name: 'New Doc')
 
-  	expect([document[:edit_id], document[:view_id], document[:template_id]]).to all satisfy { |value| value.length == 30 }
+  	expect([document[:edit_id], document[:view_id], document[:template_id]]).to all match(/[a-z]{30}/)
   end
 end
