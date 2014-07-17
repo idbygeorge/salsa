53b952a9a0489677b20005ea0a3ae7e44229c68e
John Pope <github@popeit.com>
2014-07-17 01:50:14 -0600

    document model needs to be able to regenerate IDs

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Document, :type => :model do
 
   	expect([document[:edit_id], document[:view_id], document[:template_id]]).to all match(/[a-z]{30}/)
   end
+
+  it "can regenerate new random IDs" do
+  	document = Document.create!(name: 'New Doc')
+  	edit_id = document[:edit_id]
+  	view_id = document[:view_id]
+  	template_id = document[:template_id]
+
+  	document.reset_ids
+
+  	expect(document[:edit_id]).not_to eq(edit_id)
+  	expect(document[:view_id]).not_to eq(view_id)
+  	expect(document[:template_id]).not_to eq(template_id)
+  	
+  	expect([document[:edit_id], document[:view_id], document[:template_id]]).to all match(/[a-z]{30}/)
+  end
 end
