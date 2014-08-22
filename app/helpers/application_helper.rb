12529da5e818a1d50d826705598f7f6866fb1429
John Pope <john.pope@usu.edu>
2014-08-22 16:21:05 -0600

    break out the home page, allow salsa_partial to use folders, move analytics and home footer into instances folder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
   def salsa_partial(name)
+    path_info = name.split '/'
+
+    path = ''
+    partial = name
+
+    if path_info.size > 1 then
+      partial = path_info.pop
+      path = path_info.join('/') + '/'
+    end
+
     #phase 2, make this dynamic?
     #dynamic scss/erb - http://microblog.anthonyestebe.com/2014-04-28/compile-your-scss-according-to-your-model-on-rails/
     #need a way to control who can do this though... or how much?
@@ -8,11 +18,11 @@ module ApplicationHelper
     if @document && @document.component && @document.component[name]
       output = @document.component[name]
     # if there is a customized partial for this organization, use that
-    elsif @view_folder && File.exists?("app/views/#{@view_folder}/_#{name}.html.erb")
-      output = render partial: "#{@view_folder}/#{name}"
+    elsif @view_folder && File.exists?("app/views/#{@view_folder}/#{path}_#{partial}.html.erb")
+      output = render partial: "#{@view_folder}/#{path}#{partial}"
     # otherwise, show the default if it exists
-    elsif File.exists?("app/views/instances/default/_#{name}.html.erb")
-      output = render partial: "instances/default/#{name}"
+    elsif File.exists?("app/views/instances/default/#{path}_#{partial}.html.erb")
+      output = render partial: "instances/default/#{path}#{partial}"
     # do nothing (this allows there to be additional configs added that don't have a default)
     else
       output = ''
