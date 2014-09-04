c7bf4b281f1265962bede626ab587bfef2e80438
John Pope <john.pope@usu.edu>
2014-09-04 13:06:33 -0600

    admin needs search feature

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,4 +4,8 @@ class AdminController < ApplicationController
 
     redirect_to root_path;
   end
+
+  def search page=params[:page], per=25
+    @documents = Document.where("lms_course_id = '#{params[:q]}' OR name LIKE '%#{params[:q]}%' OR edit_id LIKE '#{params[:q]}%' OR view_id LIKE '#{params[:q]}%' OR template_id LIKE '#{params[:q]}%' OR payload LIKE '%#{params[:q]}%'").page(page).per(per)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Salsa::Application.routes.draw do
   get '/:alias/:document/:action', to: redirect('/SALSA/%{document}/%{action}'), constraints: { alias: /(syllabuses|salsas?)/, action: /(edit|template)?/ }
 
   scope 'admin' do
+    get "search", to: 'admin#search', as: 'admin_search'
+
     resources :documents, as: 'admin_document', controller: 'admin_documents'
 
     post "organizations/documents"
