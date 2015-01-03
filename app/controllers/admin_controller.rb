f596bc6d9a82a3713328566e4ae3c2eac430feee
John Pope <git@popeit.com>
2015-01-03 01:50:04 -0700

    dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -28,7 +28,25 @@ class AdminController < ApplicationController
     org_slug = request.env['SERVER_NAME']
     @org = Organization.find_by slug: org_slug
 
-    @org_meta = OrganizationMeta.where root_id: @org['id']
+    org_meta = OrganizationMeta.where(
+      root_id: @org['id'],
+      key: [
+        'id',
+        'name',
+        'parent_account_id',
+        'root_account_id',
+        'sis_account_id',
+        'workflow_state'
+      ]
+    )
+
+    @org_meta = PivotTable::Grid.new do |g|
+      g.source_data = org_meta
+      g.column_name = :key
+      g.row_name = :lms_organization_id
+    end
+
+    @org_meta.build
 
     render 'admin/canvas/accounts'
   end
@@ -37,7 +55,107 @@ class AdminController < ApplicationController
     org_slug = request.env['SERVER_NAME']
     @org = Organization.find_by slug: org_slug
 
-    @document_meta = DocumentMeta.where root_organization_id: @org['id']
+query_string =
+<<SQL
+  SELECT DISTINCT a.course_id as 'id', account_id, name, course_code, enrollment_term_id, sis_course_id, start_at, end_at, workflow_state
+  FROM (
+    SELECT
+      dm.value as 'account_id',
+      dm.lms_course_id as 'course_id'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'account_id'
+  ) as a
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'name'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'name'
+  ) as n ON (a.course_id = n.course_id)
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'course_code'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'course_code'
+  ) as cc ON (a.course_id = cc.course_id)
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'enrollment_term_id'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'enrollment_term_id'
+  ) as et ON (a.course_id = et.course_id)
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'sis_course_id'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'sis_course_id'
+  ) as sis ON (a.course_id = sis.course_id)
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'start_at'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'start_at'
+  ) as start ON (a.course_id = start.course_id)
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'end_at'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'end_at'
+  ) as end ON (a.course_id = end.course_id)
+  LEFT JOIN (
+    SELECT
+      dm.lms_course_id as 'course_id',
+      dm.value as 'workflow_state'
+    FROM document_meta as dm
+    WHERE
+    root_organization_id = :root_organization_id
+    AND dm.key = 'workflow_state'
+  ) as ws ON (a.course_id = ws.course_id)
+SQL
+
+    @document_meta = DocumentMeta.find_by_sql query_string, { root_organization_id: @org[:id]}
+
+    # document_meta = DocumentMeta.where(
+    #   root_organization_id: @org['id'],
+    #   key: [
+    #     'account_id',
+    #     'course_code',
+    #     'end_at',
+    #     'enrollment_term_id',
+    #     'id',
+    #     'name',
+    #     'sis_course_id',
+    #     'start_at',
+    #     'workflow_state'
+    #   ]
+    # )
+
+    # @document_meta = PivotTable::Grid.new do |g|
+    #   g.source_data = document_meta
+    #   g.column_name = :key
+    #   g.row_name = :lms_course_id
+    # end
+    #
+    # @document_meta.build
 
     render 'admin/canvas/courses'
   end
@@ -86,7 +204,6 @@ class AdminController < ApplicationController
 
       if @canvas_client
         canvas_accounts = OrganizationMeta.where(root_id: @org['id'], key: ['id', 'parent_id']).order :key
-        debugger
         sync_canvas_courses canvas_accounts, @org[:id]
       else
         debugger
@@ -123,13 +240,13 @@ class AdminController < ApplicationController
 
   def sync_canvas_courses accounts, root_org_id
     accounts.each do |account_meta|
-      if account_meta.key == 'id'
+      if account_meta.key == 'id' then
         account = account_meta[:value]
-      elsif account_meta.key == 'parent_id'elsif account_meta.key == 'parent_id'elsif account_meta.key == 'parent_id'
+      elsif account_meta.key == 'parent_id'
         account_parent = account_meta[:value]
       end
 
-      if account_meta.key == 'id'
+      if account_meta.key == 'id' then
         # get all courses for the current acocunt
         canvas_courses = @canvas_client.get("/api/v1/accounts/#{account}/courses?per_page=50&with_enrollments=true")
         #canvas_courses.next_page! while @canvas_courses.more?
