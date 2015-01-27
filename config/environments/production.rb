98463222b864ccd2a7452a7aa99d92e19c48a6e0
John Pope <git@popeit.com>
2015-01-26 21:38:06 -0700

    precompile css and js for dashboard

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Salsa::Application.configure do
   config.assets.precompile += %w( home.js )
   config.assets.precompile += %w( admin.js )
   config.assets.precompile += %w( admin.css )
+  config.assets.precompile += %w( dashboard.css )
+  config.assets.precompile += %w( dashboard.js )
   #config.assets.precompile += ['*.js', '*.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
