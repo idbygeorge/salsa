255a6d8f15580ccf3d25a237246cc2a80eda55ce
John Pope <git@popeit.com>
2015-02-19 21:19:05 -0700

    allow admins to search their orgs

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -319,7 +319,7 @@ class AdminController < ApplicationController
 
   def search page=params[:page], per=25
 
-    @documents = Document.where("lms_course_id = '#{params[:q]}' OR name LIKE '%#{params[:q]}%' OR edit_id LIKE '#{params[:q]}%' OR view_id LIKE '#{params[:q]}%' OR template_id LIKE '#{params[:q]}%' OR payload LIKE '%#{params[:q]}%'").page(page).per(per)
+    @documents = Document.where("organization_id IN (#{@organizations.pluck(:id).join(',')}) AND (lms_course_id = '#{params[:q]}' OR name LIKE '%#{params[:q]}%' OR edit_id LIKE '#{params[:q]}%' OR view_id LIKE '#{params[:q]}%' OR template_id LIKE '#{params[:q]}%' OR payload LIKE '%#{params[:q]}%')").page(page).per(per)
   end
 
 
