4de19549d37b12fcb92a52372598eb51ab51aa29
John Pope <john@pope.tech>
2017-08-14 07:19:59 -0600

    update archive .zip to track more than one at a time (per report/org)

--- app/controllers/admin/auditor_controller.rb
+++ app/controllers/admin/auditor_controller.rb
@@ -9,6 +9,7 @@ class Admin::AuditorController < ApplicationController
     @organization = get_org
     default_term = 'SP17'
     reportJSON = nil
+    report_id = ''
 
     reports = ReportArchive.where(organization_id: @organization.id).all
 
@@ -22,6 +23,8 @@ class Admin::AuditorController < ApplicationController
     end
 
     if reportJSON
+      report_id = reportJSON.id
+
       report = JSON.parse(reportJSON.payload)
       courses = report.map{ |x|  x['course_id'] }
       docs = Document.where(
@@ -48,17 +51,24 @@ class Admin::AuditorController < ApplicationController
         # - The name of the file as it will appear in the archive
         # - The original file, including the path to find it
         rendered_doc = render_to_string :layout => "archive", :template => "documents/content"
-        zipfile.get_output_stream("#{@document.id}.html") { |os| os.write rendered_doc }
+
+        lms_identifier = 'none'
+        if @document.lms_course_id
+          lms_identifier = "#{@document.lms_course_id}"
+        end
+
+        zipfile.get_output_stream("#{lms_identifier}_#{@document.id}.html") { |os| os.write rendered_doc }
       end
     end
 
-    redirect_to '/admin/download'
+    redirect_to '/admin/download?report='+report_id
   end
 
   def download
     if File.file?(get_archive_file)
       send_file get_archive_file
-    else
+    elsif params
+
       redirect_to admin_auditor_archive_path
     end
   end
@@ -156,6 +166,11 @@ class Admin::AuditorController < ApplicationController
   private
 
   def get_archive_file
-    '/tmp/report_archive2.zip'
+    slug = get_org_slug
+    if !params[:report] || params[:report] == ''
+      params[:report] = 'default'
+    end
+
+    "/tmp/#{slug}_#{params[:report]}.zip"
   end
 end
