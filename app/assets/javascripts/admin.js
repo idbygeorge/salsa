0a23d285be966307617c7b36d34463211723d9b4
John Pope <john@pope.tech>
2017-12-13 21:22:19 -0700

    use cdn for ace

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -8,8 +8,6 @@ $(function(){
   $('#toggleChecked').on('click', function() {
     var isChecked = $('#salsaDocuments :checkbox:first').is(':checked');
 
-    console.log($('#salsaDocuments :checkbox:first').prop('checked'));
-
     if(isChecked) {
       $('#salsaDocuments :checkbox').prop('checked', false);
     } else {
