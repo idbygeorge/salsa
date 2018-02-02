14d4e55dcc14c996204a7cadd2022653e7573930
keith-ferney <keith@ferney.org>
2018-02-02 13:38:30 -0700

    token generation

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require_relative "../lib/canvas_api.rb"
 
 require 'rails/all'
 
@@ -10,7 +11,8 @@ module Salsa
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths += %W(#{config.root}/lib)
+    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
