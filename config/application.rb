4d1638a1ec533fd495432281d61a9249838dec3e
keith-ferney <keith@ferney.org>
2018-02-02 13:08:03 -0700

    token generation

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Salsa
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths += %W(#{config.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
