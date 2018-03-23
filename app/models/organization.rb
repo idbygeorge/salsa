1820f12fafe85bf2704b5924307ef361e7583e78
keith-ferney <keith@ferney.org>
2018-03-23 15:13:30 -0600

    cleanup

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,6 +6,5 @@ class Organization < ApplicationRecord
 
   default_scope { order('lft, rgt') }
   validates :slug, presence: true
-  validates :slug, exclusion: { in: %w(status), message: "%{value} is reserved." }
   validates :name, presence: true
 end
