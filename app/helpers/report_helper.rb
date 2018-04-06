e59ca51d7c925cdfc513e8b8ec6969ee4c401a4f
keith-ferney <keith@ferney.org>
2018-04-06 10:10:00 -0600

    work around  for bug on stage

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -49,20 +49,20 @@ module ReportHelper
       document_metas = {}
       docs.each do |doc|
         @document = doc
+        lms_identifier = @document.name.gsub(/[^A-Za-z0-9]+/, '_')
+        if @organization.track_meta_info_from_document
+          dm = "#{DocumentMeta.where("key LIKE :prefix AND document_id IN (:document_id)", prefix: "salsa_%", document_id: doc.id).select(:key, :value).to_json(:except => :id)}"
+          document_metas["lms_course-#{@document.lms_course_id}"] = JSON.parse(dm)
+          zipfile.get_output_stream("#{lms_identifier}_#{@document.id}_document_meta.json"){ |os| os.write JSON.pretty_generate(JSON.parse(dm))   }
+        end
         # Two arguments:
         # - The name of the file as it will appear in the archive
         # - The original file, including the path to find it
         #rendered_doc = render_to_string :layout => "archive", :template => "documents/content"
         rendered_doc = ApplicationController.new.render_to_string(layout: 'archive',partial: 'documents/content', locals: {doc: @document, organization: @organization})
 
-        lms_identifier = @document.name.gsub(/[^A-Za-z0-9]+/, '_')
         if @document.lms_course_id
           lms_identifier = "#{@document.lms_course_id}".gsub(/[^A-Za-z0-9]+/, '_')
-        end
-        if @organization.track_meta_info_from_document
-          dm = "#{DocumentMeta.where("key LIKE :prefix AND document_id IN (:document_id)", prefix: "salsa_%", document_id: doc.id).select(:key, :value).to_json(:except => :id)}"
-          document_metas["lms_course-#{@document.lms_course_id}"] = JSON.parse(dm)
-          zipfile.get_output_stream("#{lms_identifier}_#{@document.id}_document_meta.json"){ |os| os.write JSON.pretty_generate(JSON.parse(dm))   }
         end
         zipfile.get_output_stream("#{lms_identifier}_#{@document.id}.html") { |os| os.write rendered_doc }
       end
