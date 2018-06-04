27be92996aa7c076f03dd96db101cfb66fa7fe07
keith-ferney <keith@ferney.org>
2018-06-04 11:15:39 -0600

    zipfile stored in shared tmp folder

--- app/controllers/admin/auditor_controller.rb
+++ app/controllers/admin/auditor_controller.rb
@@ -5,71 +5,10 @@ class Admin::AuditorController < ApplicationController
 
   before_action :require_auditor_role
 
-  def archive
-    @organization = get_org
-    default_term = 'SP17'
-    reportJSON = nil
-    report_id = ''
-
-    reports = ReportArchive.where(organization_id: @organization.id).all
-
-    reports.each do |r|
-      if @organization.default_account_filter
-        default_term = @organization.default_account_filter
-      end
-      if r.report_filters && r.report_filters["account_filter"] == default_term
-        reportJSON = r
-      end
-    end
-
-    if reportJSON
-      report_id = reportJSON.id
-
-      report = JSON.parse(reportJSON.payload)
-      courses = report.map{ |x|  x['course_id'] }
-      docs = Document.where(
-          lms_course_id: courses
-        ).where(
-          organization_id: @organization.id
-        ).all
-    else
-      docs = Document.where(organization_id: @organization.id ).all
-    end
-
-    if File.file?(get_archive_file)
-      File.delete(get_archive_file)
-    end
-
-    zipfile_name = get_archive_file
-
-    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-      zipfile.get_output_stream('content.css'){ |os| os.write Rails.application.assets['application.css'].to_s }
-      docs.each do |doc|
-
-        @document = doc
-        # Two arguments:
-        # - The name of the file as it will appear in the archive
-        # - The original file, including the path to find it
-        rendered_doc = render_to_string :layout => "archive", :template => "documents/content"
-
-        lms_identifier = @document.name.parameterize
-        if @document.lms_course_id
-          lms_identifier = "#{@document.lms_course_id}".parameterize
-        end
-
-        zipfile.get_output_stream("#{lms_identifier}_#{@document.id}.html") { |os| os.write rendered_doc }
-      end
-    end
-
-    redirect_to '/admin/download?report='+"#{report_id}"
-  end
-
   def download
-    if File.file?(get_archive_file)
-      send_file get_archive_file
-    elsif params
-
-      redirect_to admin_auditor_archive_path
+    zipfile_path = ReportHelper.zipfile_path(get_org_slug, params[:report_id])
+    if File.file?(zipfile_path)
+      send_file (zipfile_path)
     end
   end
 
@@ -108,10 +47,6 @@ class Admin::AuditorController < ApplicationController
 
         end
       end
-    end
-
-    if File.file?(get_archive_file)
-      @download_snapshot = true
     end
 
     render 'reports', layout: '../admin/auditor/report_layout'
@@ -182,12 +117,4 @@ class Admin::AuditorController < ApplicationController
 
   private
 
-  def get_archive_file
-    slug = get_org_slug
-
-    if !params[:report] || params[:report] == ''
-      params[:report] = 'default'
-    end
-    "/tmp/#{slug}_#{params[:report]}.zip"
-  end
 end
