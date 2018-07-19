757647bab4866f363cab67fb389a6ac49ef8330e
keith-ferney <keith@ferney.org>
2018-07-19 15:08:04 -0600

    document should save checked state for radio and checkbox

--- app/assets/javascripts/on_change.js
+++ app/assets/javascripts/on_change.js
@@ -7,8 +7,16 @@
     $("#page").find( ':input:text').on("change", function(){
       $(this).attr("value",$(this).val())
     });
-    $("#page").find( ':input:radio,:checkbox').on("change", function(){
-      $(this).is(':checked')
+    $("#page").find( ':input:checkbox').on("change", function(){
+      if($(this).is(':checked')){
+        $(this).attr("checked",true)
+      } else {
+        $(this).removeAttr("checked",true)
+      }
+    });
+    $("#page").find( ':input:radio').on("change", function(){
+      $(this).attr("checked",true)
+      $(this).siblings("[name="+$(this).attr("name")+"]").removeAttr("checked")
     });
   });
 })(jQuery);
