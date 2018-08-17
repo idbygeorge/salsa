38cd03bbee8d3623363da8af60f3fe4676cc7eec
keith-ferney <keith@ferney.org>
2018-08-17 14:43:15 -0600

    periods views should not be broken and the workflow mailer should have a url variable

--- app/controllers/workflow_documents_controller.rb
+++ app/controllers/workflow_documents_controller.rb
@@ -35,7 +35,7 @@ class WorkflowDocumentsController < ApplicationController
       wfs = WorkflowStep.find(params[:document][:workflow_step_id])
       if wfs.step_type == "start_step"
         user = User.find(params[:document][:user_id])
-        WorkflowMailer.welcome_email(user,@organization,wfs.slug,component_allowed_liquid_variables(@document.workflow_step,User.find(params[:document][:user_id]),@organizaiton)).deliver_later
+        WorkflowMailer.welcome_email(user,@organization,wfs.slug,component_allowed_liquid_variables(@document.workflow_step,User.find(params[:document][:user_id]),@organization)).deliver_later
       end
     end
 
--- app/mailers/workflow_mailer.rb
+++ app/mailers/workflow_mailer.rb
@@ -1,6 +1,7 @@
 class WorkflowMailer < ApplicationMailer
   def step_email user, organization, step_slug, allowed_variables
     orgs = organization.parents.push(organization)
+    allowed_variables["workflow_documents_url"] = workflow_document_index_url(host: organization.slug)
     workflow_step = WorkflowStep.find_by(organization_id: orgs.map(&:id), slug: step_slug)
     @mail_component = Component.find_by(organization_id: orgs.map(&:id), category: "mailer", slug: "#{step_slug}_mailer", format: "liquid")
     @next_component = Component.find_by(organization_id: orgs.map(&:id), slug: WorkflowStep.find_by(organization_id: orgs.map(&:id), id:workflow_step&.next_workflow_step_id).slug) if workflow_step&.next_workflow_step_id
@@ -18,10 +19,11 @@ class WorkflowMailer < ApplicationMailer
 
   def welcome_email user, organization, step_slug, allowed_variables
     orgs = organization.parents.push(organization)
+    allowed_variables["workflow_documents_url"] = workflow_document_index_url(host: organization.slug)
     @mail_component = Component.find_by(organization_id: orgs.map(&:id),category: "mailer", slug: "workflow_welcome_email", format: "liquid")
     if @mail_component
       @template = Liquid::Template.parse(@mail_component.layout)
-      @welcome_email = @template.render(allowed_variables)
+      @welcome_email = @template.render(allowed_variables).html_safe
       @subject = Liquid::Template.parse(@mail_component.subject).render(allowed_variables).html_safe
       mail(to: user.email, subject: @subject)
     end
