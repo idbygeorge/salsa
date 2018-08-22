bac545a632a89a49cc20dc149e587aa01b7d6edd
keith-ferney <keith@ferney.org>
2018-08-22 15:56:06 -0600

    force_https should not create a redirect loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :redirect_if_user_archived, except:[:logout]
-  force_ssl if :ssl_enabled?
+  force_ssl if: :ssl_enabled?
 
   include ApplicationHelper
 
